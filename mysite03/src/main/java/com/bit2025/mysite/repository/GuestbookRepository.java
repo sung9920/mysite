72e7a933b816eb1eb425d9a15c1589d85ec76720
sung9920 <sungcheol9920@gmail.com>
2025-09-05 11:49:36 +0900

    mybatis #2

--- mysite03/src/main/java/com/bit2025/mysite/repository/GuestbookRepository.java
+++ mysite03/src/main/java/com/bit2025/mysite/repository/GuestbookRepository.java
@@ -1,11 +1,7 @@
 package com.bit2025.mysite.repository;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import javax.sql.DataSource;
 
@@ -26,68 +22,14 @@ public class GuestbookRepository {
 	private SqlSession sqlSession;
 
 	public List<GuestbookVo> findAll() throws GuestbookRepositoryException {
-		List<GuestbookVo> result = new ArrayList<>();
-
-		try (
-			Connection conn = dataSource.getConnection();
-			PreparedStatement pstmt = conn.prepareStatement("select id, name, message, date_format(reg_date, '%Y-%m-%d %h:%i:%s') from guestbook order by reg_date desc");
-			ResultSet rs = pstmt.executeQuery();
-		) {
-			while(rs.next()) {
-				Long id = rs.getLong(1);
-				String name = rs.getString(2);
-				String message = rs.getString(3);
-				String regDate = rs.getString(4);
-
-				GuestbookVo vo = new GuestbookVo();
-				vo.setId(id);
-				vo.setName(name);
-				vo.setMessage(message);
-				vo.setRegDate(regDate);
-
-				result.add(vo);
-			}
-		} catch (SQLException e) {
-			throw new GuestbookRepositoryException(e.toString());
-		}
-
-		return result;
+		return sqlSession.selectList("guestbook.findAll");
 	}
 
 	public int insert(GuestbookVo vo) {
-		int count = 0;
-
-		try (
-			Connection conn = dataSource.getConnection();
-			PreparedStatement pstmt = conn.prepareStatement("insert into guestbook values(null, ?, ?, ?, now())");
-		) {
-			pstmt.setString(1, vo.getName());
-			pstmt.setString(2, vo.getPassword());
-			pstmt.setString(3, vo.getMessage());
-
-			count = pstmt.executeUpdate();
-		} catch (SQLException e) {
-			throw new GuestbookRepositoryException(e.toString());
-		}
-
-		return count;
+		return sqlSession.insert("guestbook.insert", vo);
 	}
 
 	public int deleteByIdAndPassword(Long id, String password) {
-		int count = 0;
-
-		try (
-			Connection conn = dataSource.getConnection();
-			PreparedStatement pstmt = conn.prepareStatement("delete from guestbook where id=? and password=?");
-		) {
-			pstmt.setLong(1, id);
-			pstmt.setString(2, password);
-
-			count = pstmt.executeUpdate();
-		} catch (SQLException e) {
-			throw new GuestbookRepositoryException(e.toString());
-		}
-
-		return count;
+		return sqlSession.delete("guestbook.deleteByIdAndPassword", Map.of("id", id, "password", password));
 	}
 }
