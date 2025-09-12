c1a6e37a4d1956cb35cb4dd2e39ca89ef863f40a
sung9920 <sungcheol9920@gmail.com>
2025-09-12 11:11:48 +0900

    mysite03 site #2

--- mysite03/src/main/java/com/bit2025/mysite/controller/MainController.java
+++ mysite03/src/main/java/com/bit2025/mysite/controller/MainController.java
@@ -4,24 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.bit2025.mysite.service.SiteService;
-import com.bit2025.mysite.vo.SiteVo;
 import com.bit2025.mysite.vo.UserVo;
 
 @Controller
 public class MainController {
-	private SiteService siteService;
-
-	public MainController(SiteService siteService) {
-		this.siteService = siteService;
-	}
-
 	@RequestMapping({"/", "/main"})
 	public String index(Model model) {
-		SiteVo siteVo = siteService.getSite();
-		model.addAttribute("siteVo", siteVo);
-
 		return "main/index";
 	}
 
--- mysite03/src/main/java/com/bit2025/mysite/interceptor/SiteInterceptor.java
+++ mysite03/src/main/java/com/bit2025/mysite/interceptor/SiteInterceptor.java
@@ -3,17 +3,29 @@ package com.bit2025.mysite.interceptor;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import com.bit2025.mysite.service.SiteService;
+import com.bit2025.mysite.vo.SiteVo;
 
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class SiteInterceptor implements HandlerInterceptor {
+	private ServletContext servletContext;
 	private SiteService siteService;
-	
-	public SiteIntercecptor 
-	
+
+	public SiteInterceptor(ServletContext servletContext, SiteService siteService) {
+		this.servletContext = servletContext;
+		this.siteService = siteService;
+	}
+
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		SiteVo siteVo = (SiteVo)servletContext.getAttribute("siteVo");
+
+		if(siteVo == null) {
+			siteVo = siteService.getSite();
+			servletContext.setAttribute("siteVo", siteVo);
+		}
 
 		return true;
 	}
